3b3ae4d64e17dd72665f09e25f4ab08435e425df
Parul-Concetto <parul.bava@concettolabs.com>
2023-11-29 19:06:08 +0530

    - created ListView

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
