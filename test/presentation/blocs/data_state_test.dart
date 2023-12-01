be37583cefad677c06c7a90143380e1d3dc70fd6
Parul-Concetto <parul.bava@concettolabs.com>
2023-12-01 10:50:26 +0530

    - code refactor

--- test/presentation/blocs/data_state_test.dart
+++ test/presentation/blocs/data_state_test.dart
@@ -3,35 +3,32 @@ import 'package:santa_app/presentation/blocs/data_bloc/data_state.dart';
 
 void main() {
   group("DataState", () {
-    group('DataState', () {
-      test('InitialDataState should be equal', () {
-        expect(InitialDataState(), InitialDataState());
-      });
+    test('InitialDataState should be equal', () {
+      expect(InitialDataState(), InitialDataState());
+    });
 
-      test('LoadingDataState should be equal', () {
-        expect(LoadingDataState(), LoadingDataState());
-      });
+    test('LoadingDataState should be equal', () {
+      expect(LoadingDataState(), LoadingDataState());
+    });
 
-      test('SuccessDataState should be equal', () {
-        expect(SuccessDataState(), SuccessDataState());
-      });
+    test('SuccessDataState should be equal', () {
+      expect(SuccessDataState(), SuccessDataState());
+    });
 
-      test('StatusChange should be equal', () {
-        expect(StatusChange(), StatusChange());
-      });
+    test('StatusChange should be equal', () {
+      expect(StatusChange(), StatusChange());
+    });
 
-      test('ErrorDataState should be equal with the same errorMessage', () {
-        expect(ErrorDataState(errorMessage: 'Error 1'),
-            ErrorDataState(errorMessage: 'Error 1'));
-      });
+    test('ErrorDataState should be equal with the same errorMessage', () {
+      expect(ErrorDataState(errorMessage: 'Error 1'),
+          ErrorDataState(errorMessage: 'Error 1'));
+    });
 
-      test('ErrorDataState should not be equal with different errorMessage',
-          () {
-        expect(
-          ErrorDataState(errorMessage: 'Error 1'),
-          ErrorDataState(errorMessage: 'Error 2'),
-        );
-      });
+    test('ErrorDataState should not be equal with different errorMessage', () {
+      expect(
+        ErrorDataState(errorMessage: 'Error 1'),
+        ErrorDataState(errorMessage: 'Error 2'),
+      );
     });
   });
 }
